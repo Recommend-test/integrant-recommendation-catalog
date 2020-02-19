36a69a861b9dcc172e25b8cf26dddd391937ff0f
Elsayed.Hamed <Elsayed.Hamed@host.docker.internal>
2020-02-19 15:04:50 +0200

    remove unused imports

--- src/main/java/com/integrant/recommendation/product/model/Product.java
+++ src/main/java/com/integrant/recommendation/product/model/Product.java
@@ -16,7 +16,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 /**
